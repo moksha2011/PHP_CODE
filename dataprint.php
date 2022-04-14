70465bcee2e8b85b329a8b998f2b579d7bf11855
moksha2011 <68639524+moksha2011@users.noreply.github.com>
2022-04-14 12:09:20 +0530

    add file

---
+++ dataprint.php
@@ -0,0 +1,6 @@
+<?php
+	$email = $_GET['emid'];
+	$password = $_GET['passwd'];
+	echo "email : ".$email."<br>";
+	echo "password : ".$password;
+?>
---
+++ index.php
@@ -0,0 +1,12 @@
+<html>
+        <head>
+                <title>Demo</title>
+        </head>
+        <body>
+                <form method="get" action="dataprint.php">
+                        email:<input type="text" name="emid"><br>
+                        password:<input type="text" name="passwd"><br>
+                        <input type="submit" name="submit" value="submit">
+                </form>
+        </body>
+</html>
